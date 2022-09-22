7fac6cef0678d6cf8561992f41323d5ba40691e9
dreamscached <dreamscache.d@gmail.com>
2022-09-22 10:21:45 +0200

    Add error check

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,15 @@
 // Created by dreamscached on 21.9.22.
 //
 
+#include <iostream>
 #include "urlopen.hpp"
 #include "const.hpp"
 
 int main(int argc, char **argv) {
-    urlopen(RRCXX_RICK_URL);
+    if (!urlopen(RRCXX_RICK_URL)) {
+        std::cout << "error: could not open video url." << std::endl;
+        return 1;
+    }
+
+    return 0;
 }
