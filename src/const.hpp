46a3b565cf5f3c398f901b2b590889a01fafa89c
dreamscached <dreamscache.d@gmail.com>
2022-09-24 16:22:28 +0200

    Fix swapped URLs

--- src/const.hpp
+++ src/const.hpp
@@ -6,9 +6,9 @@
 #define RICKROLLCXX_CONST_HPP
 
 #if defined(NOADS_URL)
-#define RRCXX_RICK_URL "https://youtu.be/dQw4w9WgXcQ"
-#else
 #define RRCXX_RICK_URL "https://youtu.be/xvFZjo5PgG0"
+#else
+#define RRCXX_RICK_URL "https://youtu.be/dQw4w9WgXcQ"
 #endif
 
 #endif //RICKROLLCXX_CONST_HPP
