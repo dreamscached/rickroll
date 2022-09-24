40b0668632b197efb820ff9695374c500e1d7983
dreamscached <dreamscache.d@gmail.com>
2022-09-24 16:05:14 +0200

    Fix error message

--- src/urlopen_linux.cpp
+++ src/urlopen_linux.cpp
@@ -9,7 +9,7 @@
 
 bool urlopen(const std::string &url) {
     // FIXME: this isn't reliable, need to cli encode this so this prevents globbing
-    return system(("xdg-open " + url).c_str()) != 0;
+    return system(("xdg-open " + url).c_str()) == 0;
 }
 
 #endif
