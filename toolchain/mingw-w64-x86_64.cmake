dacc7304d0a69cb93953edd04c72f2bb27ca5d66
dreamscached <dreamscache.d@gmail.com>
2022-09-24 23:42:32 +0200

    Add toolchain for mingw i686

--- toolchain/mingw-w64-x86_64.cmake
+++ toolchain/mingw-w64-x86_64.cmake
@@ -9,14 +9,11 @@
 set(CMAKE_SYSTEM_NAME Windows)
 set(TOOLCHAIN_PREFIX x86_64-w64-mingw32)
 
-# Cross compilers to use for C and C++.
 set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
 
-# Target environment on the build host system.
 set(CMAKE_FIND_ROOT_PATH /usr/${TOOLCHAIN_PREFIX})
 
-# Modify default behavior of FIND_XXX() commands.
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
