e2cbf2bd2e54d97e236dda6aaaa4a248fe412093
dreamscached <dreamscache.d@gmail.com>
2022-09-26 08:38:59 +0200

    Implement with execvp

--- src/urlopen_linux.cpp
+++ src/urlopen_linux.cpp
@@ -5,6 +5,7 @@
 #ifdef __linux__
 
 #include <cstdlib>
+#include <vector>
 #include <unistd.h>
 #include <sys/wait.h>
 #include "urlopen.hpp"
@@ -17,10 +18,17 @@ bool urlopen(const std::string &url) {
     }
 
     if (pid == 0) {
-        // TODO: Would better use an array and an execvp rather than this way of duplication.
-        // This needs duplicated xdg-open string because of argv convention of having argv[0] a program name.
-        if (execlp("xdg-open", "xdg-open", url.c_str()) == -1) {
-            perror("execlp");
+        const std::vector<std::string> args{"xdg-open", url};
+        std::vector<const char*> argv;
+
+        for (const std::string &s : args) {
+            argv.push_back(s.data());
+        }
+        argv.push_back(nullptr);
+        argv.shrink_to_fit();
+
+        if (execvp(argv[0], const_cast<char* const*>(argv.data())) == -1) {
+            perror("execvp");
             exit(1);
         }
 
