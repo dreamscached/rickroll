b25ddc7177530f5f08e1364a417052e572bef4c4
dreamscached <dreamscache.d@gmail.com>
2022-09-29 13:18:39 +0200

    Add CLI11

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,14 @@
 //
 
 #include <iostream>
+#include <CLI/CLI.hpp>
 #include "urlopen.hpp"
 #include "const.hpp"
 
 int main(int argc, char **argv) {
+    CLI::App app{"open a rickroll link in default browser"};
+    CLI11_PARSE(app, argc, argv)
+
     if (!urlopen(RRCXX_RICK_URL)) {
         std::cerr << "error: could not open video url." << std::endl;
         return 1;
